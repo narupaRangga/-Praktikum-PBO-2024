d0c1f8b0d238962a995efaceee85f8a08e75ccc9
narupaRangga <145355546+narupaRangga@users.noreply.github.com>
2024-03-11 23:52:50 +0800

    Add files via upload

---
+++ pertemuan 5/POSTTEST 1/Post1/src/Posttest1/liga1.java
@@ -0,0 +1,16 @@
+package Posttest1;
+
+public class liga1 {
+    String nama_klub;
+    String asal_kota;
+    String pelatih;
+
+    public liga1(String nama_klub, String asal_kota, String pelatih) {
+        this.nama_klub = nama_klub;
+        this.asal_kota = asal_kota;
+        this.pelatih = pelatih;
+    }
+}
+
+    
+
