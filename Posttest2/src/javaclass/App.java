d1271d934dba30627701cbe4255ac26d0d2e6497
narupaRangga <145355546+narupaRangga@users.noreply.github.com>
2024-03-20 10:09:19 +0800

    POSTTEST2

---
+++ Posttest2/src/javaclass/App.java
@@ -0,0 +1,153 @@
+package javaclass;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    static Scanner ip = new Scanner(System.in);
+    static ArrayList<liga1> liga = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+
+        while (true) {
+            System.out.println("\033[H\033[2J");
+            System.out.println("=====================================");
+            System.out.println("        Pendataan Klub Liga 1");
+            System.out.println("======================================");
+            System.out.println("1. Tambah Data");
+            System.out.println("2. Lihat Data");
+            System.out.println("3. Ubah Data");
+            System.out.println("4. Hapus Data");
+            System.out.println("5. Keluar");
+            System.out.println("===================================");
+            System.out.println("");
+            System.out.print("Masukkan Pilihan Anda : ");
+            String input = ip.nextLine();
+
+            if (input.equals("1")) {
+                // Add Data
+                System.out.println("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("===================================");
+                System.out.println("            Tambah Data            ");
+                System.out.println("===================================");
+                System.out.println("");
+                System.out.print("Masukkan Nama Klub : ");
+                String liga_nama = ip.next();
+                System.out.print("Masukkan Nama kota : ");
+                String liga_kota = ip.next();
+                System.out.print("Masukkan Nama Pelatih : ");
+                String liga_pelatih = ip.next();
+                liga1 Liga = new liga1();
+                Liga.setNama_klub(liga_nama);
+                Liga.setAsal_kota(liga_kota);
+                Liga.setPelatih(liga_pelatih);
+                liga.add(Liga);
+
+
+            } else if (input.equals("2")) {
+                // View Data
+                System.out.println("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("===================================");
+                System.out.println("             Lihat Data            ");
+                System.out.println("===================================");
+                System.out.println("");
+                for (int i = 0; i < liga.size(); i++) {
+                    liga1 Liga = liga.get(i);
+                    System.out.println("===================================");
+                    System.out.println("Nama Klub    : " + Liga.getNama_klub());
+                    System.out.println("Nama Kota    : " + Liga.getAsal_kota());
+                    System.out.println("Nama Pelatih : " + Liga.getPelatih());
+                    System.out.println("===================================");
+                }
+                System.out.println("\nPress \"ENTER\" to continue...");
+                ip.nextLine();
+            } else if (input.equals("3")) {
+                // Update Data
+                System.out.println("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("===================================");
+                System.out.println("                Ubah               ");
+                System.out.println("===================================");
+                System.out.println("");
+                System.out.print("Masukkan Nama Klub : ");
+                String nama_klub = ip.next();
+                boolean ditemukan = false;
+                for (int i = 0; i < liga.size(); i++) {
+                    liga1 Liga = liga.get(i);
+                    if (Liga.getNama_klub().equalsIgnoreCase(nama_klub)) {
+                        ditemukan = true;
+                        System.out.println("\033[H\033[2J");
+                        System.out.flush();
+                        System.out.println("===================================");
+                        System.out.println("             Ubah Data             ");
+                        System.out.println("===================================");
+                        System.out.println("1. Ubah Nama Klub");
+                        System.out.println("2. Ubah Nama Kota");
+                        System.out.println("3. Ubah Nama Pelatih");
+                        System.out.println("===================================");
+                        System.out.println("");
+                        System.out.print("Masukkan Pilihan Anda: ");
+                        String input_edit = ip.next();
+
+                        if (input_edit.equals("1")) {
+                            System.out.print("Nama Klub : ");
+                            String new_nama_klub = ip.next();
+                            Liga.setNama_klub(new_nama_klub);
+                        } else if (input_edit.equals("2")) {
+                            System.out.print("Nama Kota : ");
+                            String new_nama_kota = ip.next();
+                            Liga.setAsal_kota(new_nama_kota); 
+                        } else if (input_edit.equals("3")) {
+                            System.out.print("Nama Pelatih : ");
+                            String new_nama_pelatih = ip.next();
+                            Liga.setPelatih(new_nama_pelatih);
+                        } else {
+                            System.out.println("Pilihan yang Anda Masukkan Tidak Valid");
+                            break;
+                        }
+                        System.out.println("Data berhasil diubah!");
+                        break;
+                    }
+                }
+                if (!ditemukan) {
+                    System.out.println("Data tidak ditemukan!");
+                }
+                ip.nextLine();
+                System.out.println("\nPress \"ENTER\" to continue...");
+                ip.nextLine();
+            } else if (input.equals("4")) {
+                // Delete Data
+                System.out.println("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("===================================");
+                System.out.println("             Hapus Data            ");
+                System.out.println("===================================");
+                System.out.print("Masukkan Nama Klub yang akan dihapus: ");
+                String nama_klub = ip.next();
+                boolean removed = false;
+                for (int i = 0; i < liga.size(); i++) {
+                    if (liga.get(i).getAsal_kota().equalsIgnoreCase(nama_klub)) {
+                        liga.remove(i);
+                        removed = true;
+                        System.out.println("Data berhasil dihapus!");
+                        break;
+                    }
+                }
+                if (!removed) {
+                    System.out.println("Data tidak ditemukan!");
+                }
+                ip.nextLine();
+                System.out.println("\nPress \"ENTER\" to continue...");
+                ip.nextLine();
+            } else if (input.equals("5")) {
+    
+                System.out.println("Anda Sudah Keluar...");
+                break;
+            }
+        }
+        
+        ip.close();
+    }
+}
---
+++ Posttest2/src/javaclass/liga1.java
@@ -0,0 +1,41 @@
+package javaclass;
+
+public class liga1 {
+    private String nama_klub;
+    private String asal_kota;
+    private String pelatih;
+
+    // public liga1(String nama_klub, String asal_kota, String pelatih) {
+    //     this.nama_klub = nama_klub;
+    //     this.asal_kota = asal_kota;
+    //     this.pelatih = pelatih;
+    // }
+
+    public String getNama_klub() {
+        return nama_klub;
+    }
+
+    public void setNama_klub(String nama_klub) {
+        this.nama_klub = nama_klub;
+    }
+
+    public String getAsal_kota() {
+        return asal_kota;
+    }
+
+    public void setAsal_kota(String asal_kota) {
+        this.asal_kota = asal_kota;
+    }
+
+    public String getPelatih() {
+        return pelatih;
+    }
+
+    public void setPelatih(String pelatih) {
+        this.pelatih = pelatih;
+    }
+
+}
+
+    
+
